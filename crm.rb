0260bd606563adedc7df22dd0e474e404eb57d05
KaiserChan <chankaiserkk@gmail.com>
2017-08-25 15:22:11 -0400

    Updated crm-web - updated redirecting after edit, and updated linking of the edit button @ the contact page

--- crm.rb
+++ crm.rb
@@ -55,6 +55,21 @@ get '/contacts/:id/edit' do
   end
 end
 
+put '/contacts/:id' do
+  @contacts = Contact.find_by(id: params[:id].to_i)
+  if @contacts
+    @contacts.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
 
 after do
   ActiveRecord::Base.connection.close
